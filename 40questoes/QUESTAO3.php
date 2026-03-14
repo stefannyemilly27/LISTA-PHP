e47eccc72c24d732e19e8d2220383e51d981d4b1
stefannyemilly27 <stefanny.emilly27@gmail.com>
2026-03-14 10:45:10 -0300

    php

--- 40questoes/QUESTAO3.php
+++ 40questoes/QUESTAO3.php
@@ -9,11 +9,21 @@
     <h1>Automóvel - média</h1>
     <h2>Escrever um algoritmo para determinar o consumo médio de um automóvel sendo fornecida a distância total
 percorrida pelo automóvel e o total de combustível gasto.</h2>
-</body>
 <form action="" method="GET">
     <label>Digite o combustível gasto:</label>
     <input type="number" name="combustivel" step="any" required><br><br>
     <label>Digite a distância percorrida:</label>
     <input type="number" name="distancia" step="any" required><br><br>
 </form>
+<?php
+if(isset($_GET['combustivel']) && isset ($_GET['distancia'])){
+    $combustivel = (float)$_GET['combustivel'];
+    $distancia = (float)$_GET['distancia'];
+    $comsumo = $combustivel / $distancia;
+}
+
+echo "<h2>Consumo médio: $consumo</h2>"
+
+?>
+</body>
 </html>
